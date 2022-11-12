ed71a956b8146c93016969033ebc7af8cfce1bc9
Nekotato1412 <nekotato1412@gmail.com>
2022-11-12 10:25:42 -0600

    Commented out authentication code

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,26 +3,36 @@ const loginRouter = Express.Router()
 const Account = require('../models/account')
 const { findByEmail } = require('./accounts.js')
 
-loginRouter.post("/", async (req, res) => {
-    if (req.body.password && req.body.email) {
-            const account = await findByEmail(req.body.email)
-            if (!account) {
-                return res.status(404).json({"message": "Account not found."})
-            }
+// loginRouter.post("/", async (req, res) => {
+//     if (req.body.password && req.body.email) {
+//             const account = await findByEmail(req.body.email)
+//             if (!account) {
+//                 return res.status(404).json({"message": "Account not found."})
+//             }
 
-            if (account.password == req.body.password) {
-                account.lastLogin = Date.now().toString()
-                try {
-                    await account.save()
-                } catch(err) {
-                    return res.status(500).json({"message": err.message})
-                }
-                return res.status(200).json(account)
-            } else {
-                return res.status(404).json({ "message": "Incorrect password."})
-            }
+//             if (account.password == req.body.password) {
+//                 account.lastLogin = Date.now().toString()
+//                 try {
+//                     await account.save()
+//                 } catch(err) {
+//                     return res.status(500).json({"message": err.message})
+//                 }
+//                 return res.status(200).json(account)
+//             } else {
+//                 return res.status(404).json({ "message": "Incorrect password."})
+//             }
+//     } else {
+//         res.status(401).json({ "message": "Missing data." })
+//     }
+// })
+
+// Get by Email
+loginRouter.get("/:email", async (req, res) => {
+    const account = await findByEmail(req.params.email)
+    if (!account) {
+        res.status(404).json({ "message": "Account not found." })
     } else {
-        res.status(401).json({ "message": "Missing data." })
+        res.status(200).json(account)
     }
 })
 
